10023c7b1c7721f65d8b83835776c19d4a8d9a76
egor.gututui <egor.gututui@CP-SIO-W10-PG04.bts.sio>
2023-02-03 11:13:37 +0100

    creation js tester et fichiers js alcoolPublic + testAlcoolemie

---
+++ js/alcoolPublic.js
@@ -0,0 +1,13 @@
+//0 verres 
+if(0 === getAlcoolPur(0)){
+    console.log('0 verres : ok');
+}else {
+    console.log('0 verres: erreur');
+}
+
+//1 verre 
+if(10 === getAlcoolPur(1)){
+    console.log('1 verre : ok');
+} else {
+    console.log('1 verre : erreur');
+}
