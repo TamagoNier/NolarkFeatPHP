59a9ca9455d864daf920c49c688d1e55fef3c483
egor.gututui <egor.gututui@CP-SIO-W10-PG04.bts.sio>
2023-02-09 19:43:02 +0100

    retest import export

--- js/primeChauffeurs.js
+++ js/primeChauffeurs.js
@@ -1,3 +1,5 @@
+//import recupValeur from './remuneration'
+
 /*  
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
