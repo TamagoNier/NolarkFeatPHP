2b53378300bdac9bb69e8e045b3f638a47693af8
egor.gututui <egor.gututui@CP-SIO-W10-PG04.bts.sio>
2023-02-10 10:15:04 +0100

    ajout test GetAmende et fonction getAmende

--- js/alcoolPublic.js
+++ js/alcoolPublic.js
@@ -40,3 +40,17 @@ function getAlcoolemie(sexe, poids, nbVerres) {
         return 0; //A changer peut-etre 
     }
 }
+
+/**
+ * Fonction qui retourne l'amende encourue en fonction de l'alcoolémie
+ * 
+ * @param {float} alcoolemie
+ * @returns {String}
+ */
+function getAmende(alcoolemie) {
+    if(alcoolemie < 0.8) {
+        return 'Minorée : 90€ / Forfaitaire : 135€ / Majorée : 375€';
+    }else {
+        return '4500€';
+    }
+}
