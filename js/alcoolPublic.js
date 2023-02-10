c1f35704562d0bff7ca1b5fd2850e6737e35f0c3
egor.gututui <egor.gututui@CP-SIO-W10-PG04.bts.sio>
2023-02-10 09:43:23 +0100

    amelioration de la fonction getAlcoolemie

--- js/alcoolPublic.js
+++ js/alcoolPublic.js
@@ -5,18 +5,38 @@
  * @param {int} nbVerres
  * @returns {int}
  */
-function getAlcoolPur(nbVerres){
-    return nbVerres*10;
+function getAlcoolPur(nbVerres) {
+    return nbVerres * 10;
 }
 
+/**
+ * Ponction qui donne le coeficien de diffusion en fonction du sexe
+ * 
+ * @param {String} sexe
+ * @returns {float}
+ */
 function getCoefDiffusion(sexe) {
-    if(sexe === 'homme') {
+    if (sexe === 'homme') {
         return 0.7;
-    }else {
+    } else {
         return 0.6;
     }
 }
 
-function getAlcoolemie(sexe, poids, nbVerres){
-    return (getAlcoolPur(nbVerres) / (poids * getCoefDiffusion(sexe))).toFixed(2);
+/**
+ * Fonction qui retourne l'alcoolemie en fonction du sexe, du poids
+ * et du nombre de verres ingérés
+ * 
+ * @param {string} sexe
+ * @param {int} poids
+ * @param {int} nbVerres
+ * @returns {float}
+ */
+function getAlcoolemie(sexe, poids, nbVerres) {
+    // /!\ division par 0 ! on ne veut pas casser la matrice 
+    if (poids > 0) {
+        return (getAlcoolPur(nbVerres) / (poids * getCoefDiffusion(sexe))).toFixed(2);
+    }else {
+        return 0; //A changer peut-etre 
+    }
 }
