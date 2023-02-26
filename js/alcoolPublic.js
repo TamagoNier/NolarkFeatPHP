4c6560147e4321450ea267dd65b00dccdbb1456d
egor.gututui <egor.gututui@CP-SIO-W10-PG04.bts.sio>
2023-02-26 21:55:48 +0100

    Fin du javascript

--- js/alcoolPublic.js
+++ js/alcoolPublic.js
@@ -48,8 +48,11 @@ function getAlcoolemie(sexe, poids, nbVerres) {
  * @returns {String}
  */
 function getAmende(alcoolemie) {
-    const seuil = 0.8;
-    if (alcoolemie < seuil) {
+    const seuilDelit = 0.8;
+    const seuilContra = 0.5;
+    if (alcoolemie < seuilContra) {
+        return "Pas d'amande";
+    } else if (alcoolemie < seuilDelit) {
         return 'Minorée : 90€ / Forfaitaire : 135€ / Majorée : 375€';
     } else {
         return '4500€';
@@ -63,10 +66,13 @@ function getAmende(alcoolemie) {
  * @returns {String}
  */
 function getSanction(alcoolemie) {
-    const seuil = 0.8;
-    if (alcoolemie < seuil) {
+    const seuilDelit = 0.8;
+    const seuilContra = 0.5;
+    if (alcoolemie < seuilContra) {
+        return 'Pas de sanction, bravo !'
+    } else if (alcoolemie < seuilDelit) {
         return '6 points + suspension 3 ans';
-    } else {
+    } else if (alcoolemie > seuilDelit){
         return '6 points + 2 ans de prison + suspension 3 ans + stage de sensibilisation';
     }
 }
@@ -98,13 +104,13 @@ function getString(id) {
     return window.document.querySelector(id).value;
 }
 
+window.addEventListener("load", function () {
+    window.addEventListener('reset', function () { //"Ecoute" le site afin de détécter si le form à été reinitialisé
+        window.document.querySelector('#consequances').remove();
+        window.document.querySelector('#sensibilisation').remove();
 
+    });
 
-
-//On passe aux listenners :
-
-window.addEventListener("load", function() {
-    
     // tabEvents est une collection d'évenements
     let tabEvents = ['keyup', 'click'];
     // tabInputs est une collection de <input>
@@ -113,8 +119,43 @@ window.addEventListener("load", function() {
     for (let i = 0; i < tabInputs.length; i++) {
         for (let j = 0; j < tabEvents.length; j++) {
             // Ajout des listeners sur tous les <input> des events listés dans tabEvents
-            tabInputs[i].addEventListener(tabEvents[j], ---);
-
+            tabInputs[i].addEventListener(tabEvents[j], getConsequance);
         }
-    } 
+    }
 });
+
+function getConsequance() {
+    let poids = getInt("#num_poids");
+    let nbVerres = getInt("#nb_verres");
+    let alcool;
+
+    if (document.getElementById('rd_sexehomme').checked) {
+        alcool = getAlcoolemie("homme", poids, nbVerres);
+
+    } else if (document.getElementById('rd_sexefemme').checked) {
+        alcool = getAlcoolemie("femme", poids, nbVerres);
+    }
+    let amande = getAmende(alcool);
+    let sanction = getSanction(alcool);
+
+    gestionAffichageConsequances(amande, sanction);
+}
+
+function gestionAffichageConsequances(amande, sanction) {
+    let premElH2 = window.document.querySelector('#consequances');//creation de #consequances si il n'existe pas deja 
+
+    let deuxElH2 = window.document.querySelector('#sensibilisation');//creation de #sensibilisation si il n'existe pas deja 
+
+    if (!premElH2) {
+        premElH2 = window.document.createElement('h2');
+        premElH2.id = 'consequances';
+        window.document.querySelector('#formulaire').appendChild(premElH2);
+    }
+    if (!deuxElH2) {
+        deuxElH2 = window.document.createElement('h2');
+        deuxElH2.id = 'sensibilisation';
+        window.document.querySelector('#formulaire').appendChild(deuxElH2);
+    }
+
+    premElH2.innerHTML = sanction + "\n" + amande;
+}
